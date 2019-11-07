1ec242c154fe1bb795a1909805222b4bb648e5f0
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-11-07 23:55:03 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan48-Kalkulator/src/pboif2/pkg10118062/latihan48/kalkulator/PBOIF210118062Latihan48Kalkulator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan48.kalkulator;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBOIF210118062Latihan48Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         penghitungan hsl = new penghitungan(7 ,5);
+        hsl.hasil();
+    }
+    
+}
